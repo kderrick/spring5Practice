8ec70de124bf8e262365ec4c4b65a2b1a84a6ddb
Kyle <kderrick1988@gmail.com>
2017-09-16 10:05:22 -0700

    add files

---
+++ src/main/java/com/practiceWithSpring5/spring5Practice/Spring5PracticeApplication.java
@@ -0,0 +1,12 @@
+package com.practiceWithSpring5.spring5Practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5PracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5PracticeApplication.class, args);
+	}
+}
